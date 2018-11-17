f52d726c846a22f9143dc9c90b0a3c62b36941dd
Richard <IAMRichardT@users.noreply.github.com>
2018-11-16 16:12:39 -0800

    added base module

---
+++ lua/modules/base/sh_base_config.lua
@@ -0,0 +1,24 @@
+/*
+* 	@package	core
+* 	@module		Base
+* 	@author		Richard [http://steamcommunity.com/profiles/76561198135875727]
+* 
+* 	BY MODIFYING THIS FILE -- YOU UNDERSTAND THAT THE ABOVE MENTIONED AUTHORS CANNOT BE HELD RESPONSIBLE
+* 	FOR ANY ISSUES THAT ARISE FROM MAKING ANY ADJUSTMENTS TO THIS SCRIPT. YOU UNDERSTAND THAT THE ABOVE 
+* 	MENTIONED AUTHOR CAN ALSO NOT BE HELD RESPONSIBLE FOR ANY DAMAGES THAT MAY OCCUR TO YOUR SERVER AS A 
+* 	RESULT OF THIS SCRIPT AND ANY OTHER SCRIPT NOT BEING COMPATIBLE WITH ONE ANOTHER.
+*/
+
+/*
+*	standard tables and localization
+*/
+
+rcore = rcore or { }
+
+local base 		    = rcore
+local mod           = base:modules_load( 'base' )
+local settings      = base:modules_settings( mod )
+
+/*
+*	module settings
+*/
---
+++ lua/modules/base/sv_base.lua
@@ -0,0 +1,31 @@
+/*
+* 	@package	core
+* 	@module		Base
+* 	@author		Richard [http://steamcommunity.com/profiles/76561198135875727]
+* 
+* 	BY MODIFYING THIS FILE -- YOU UNDERSTAND THAT THE ABOVE MENTIONED AUTHORS CANNOT BE HELD RESPONSIBLE
+* 	FOR ANY ISSUES THAT ARISE FROM MAKING ANY ADJUSTMENTS TO THIS SCRIPT. YOU UNDERSTAND THAT THE ABOVE 
+* 	MENTIONED AUTHOR CAN ALSO NOT BE HELD RESPONSIBLE FOR ANY DAMAGES THAT MAY OCCUR TO YOUR SERVER AS A 
+* 	RESULT OF THIS SCRIPT AND ANY OTHER SCRIPT NOT BEING COMPATIBLE WITH ONE ANOTHER.
+*/
+
+/*
+*	standard tables and localization
+*/
+
+rcore = rcore or { }
+
+local base = rcore
+
+/*
+*   modules
+*/
+
+local mod, prefix = base:modules_load( 'base', true )
+local settings = base:modules_settings( mod )
+
+/*
+*   rlib declrations
+*/
+
+local helper = rlib.h
